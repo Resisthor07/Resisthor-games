06e149a662f946e445504a95e389b48df537f3ad
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-04 20:58:15 -0300

    trocado para input o menu

--- js/eduardo.js
+++ js/eduardo.js
@@ -1,1 +1,20 @@
 //fetch("https://www.freetogame.com/api/games");
+
+let tipoCategoria = document.getElementById("acao");
+let acao = 'shooter';
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '59aba074b0msh4dcbc270724c3f0p164e04jsn23fd06b88edb',
+		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+	}
+};
+
+
+fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
