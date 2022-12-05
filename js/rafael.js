cb30e7523f0945ccf3cdabcd7dfe88c04f75fd75
Rafael Ch <rafael.cherman0@gmail.com>
2022-12-05 17:53:44 -0300

    adicionado o js

---
+++ js/rafael.js
@@ -0,0 +1,51 @@
+let botao_pc = document.getElementById("pc");
+let botao_browser = document.getElementById("browser");
+let botao_all = document.getElementById("all");
+
+botao_pc.addEventListener("click", function(){
+    api_plataforma(1)}
+    );
+botao_browser.addEventListener("click", function(){
+    api_plataforma(2)}
+    );
+botao_all.addEventListener("click", function(){
+    api_plataforma(3)}
+    );
+
+
+
+function api_plataforma(opc)
+{
+    let plat 
+    
+    if(opc==1)
+    {
+        plat = "pc";
+    }
+    if(opc==2)
+    {
+        plat = "browser";
+    }
+    if(opc==3)
+    {
+        plat = "all";
+    }
+
+    console.log(plat);
+
+    const options = {
+        method: 'GET',
+        params: {platform: plat},
+        headers: {
+          'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+
+    fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options)
+        .then((response) => {
+            response.json()
+                .then(lista => console.log(lista));
+        })
+        .catch();
+}
