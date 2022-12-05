c716eb96166654e1cb012640a434653214e6fd46
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-04 21:27:01 -0300

    fazendo js

--- js/eduardo.js
+++ js/eduardo.js
@@ -1,7 +1,28 @@
 //fetch("https://www.freetogame.com/api/games");
 
-let tipoCategoria = document.getElementById("acao");
-let acao = 'shooter';
+let bot_action = document.getElementById("bot_action");
+bot_action = addEventListener ('click', );
+
+let bot_battle = document.getElementById("bot_battle");
+bot_battle = addEventListener ('click', );
+
+let bot_figtht = document.getElementById("bot_figtht");
+bot_figtht = addEventListener ('click', );
+
+let bot_racing = document.getElementById("bot_racing");
+bot_racing = addEventListener ('click', );
+
+let bot_sci = document.getElementById("bot_sci");
+bot_sci = addEventListener ('click', );
+
+let bot_shooting = document.getElementById("bot_shooting");
+bot_shooting = addEventListener ('click', );
+
+let bot_sports = document.getElementById("bot_sports");
+bot_sports = addEventListener ('click', );
+
+let bot_strategy = document.getElementById("bot_strategy");
+bot_strategy = addEventListener ('click', );
 
 
 const options = {
