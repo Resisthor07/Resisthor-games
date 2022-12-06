47a2e6f13e6a3eff658d3faeb2033602c2630500
SamirCharleston <samircharleston@gmail.com>
2022-12-06 20:04:40 -0300

    modificada a quantidade de acesso ao dom

--- js/samir.js
+++ js/samir.js
@@ -13,20 +13,31 @@ const dadosRece = fetch('https://free-to-play-games-database.p.rapidapi.com/api/
     console.log("Erro!");
 });
 
+let listaExibida = document.getElementById("principal");
+
+let listaContainerDeJogos = [];
+
 let dadosServidor = [];
 let botaoCarregar = document.getElementById("carregar-mais");
+let botaoRemover = document.getElementById("remover");
 let primeiroCarregamentoImagem = true;
 let controle = 0;
 let filtro = "nenhum";
 
 botaoCarregar.addEventListener("click", (dados) => { carregarMais(dadosServidor, filtro) });
+botaoRemover.addEventListener("click", limparTela);
 
 function carregarMais(dados, filtro) {
 
-    let listaExibida = document.getElementById("principal");
-    let jogos = document.createElement("div");
-    listaExibida.appendChild(jogos);
-    jogos.className = "lista-jogos";
+    let containerDeJogos = {
+        jogos: document.createElement("div"),
+        linhaJogos: []
+    }
+
+    let stringHtml = "";
+
+    listaExibida.appendChild(containerDeJogos.jogos);
+    containerDeJogos.jogos.className = "lista-jogos";
 
     if (primeiroCarregamentoImagem) {
         let imagemBanner = document.getElementById("samir-imagem-principal");
@@ -37,43 +48,45 @@ function carregarMais(dados, filtro) {
 
     for (i = 0; i < 3 && controle < dados.length; i++) {
 
-        let tituloGenero = document.createElement("h3");
-        let listaNaoOrdenada = document.createElement("ul");
+        // let linhaJogos = {
+        //     tituloGenero: document.createElement("h3"),
+        //     listaNaoOrdenada: document.createElement("ul")
+        // };
 
-        jogos.appendChild(tituloGenero);
-        jogos.appendChild(listaNaoOrdenada);
-        tituloGenero.innerText = dados[i].genre;
+        // containerDeJogos.jogos.appendChild(linhaJogos.tituloGenero);
+        // containerDeJogos.jogos.appendChild(linhaJogos.listaNaoOrdenada);
+        // linhaJogos.tituloGenero.innerText = dados[i].genre;
 
         controle += 3;
 
-        if (controle >= 11) {
-            decimoItem = `<li><img src="${dados[controle].thumbnail}" alt="${dados[controle].title}"><small>${dados[controle].title}</small></li>`;
-            controle++;
+        let stringLinhas = "";
+
+        for (j = 1; j <= 3; j++) {
+
+            stringLinhas += `
+                            <li><img src="${dados[controle + j].thumbnail}" alt="${dados[controle + j].title}"><small>${dados[controle + j].title}</small></li>
+                            `;
         }
 
-        listaNaoOrdenada.innerHTML = `
-                                        <li><img src="${dados[controle].thumbnail}" alt="${dados[controle].title}"><small>${dados[controle].title}</small></li>
-                                        <li><img src="${dados[controle + 1].thumbnail}" alt="${dados[controle].title}"><small>${dados[controle].title}</small></li>
-                                        <li><img src="${dados[controle + 2].thumbnail}" alt="${dados[controle].title}"><small>${dados[controle].title}</small></li>${decimoItem}
-                                    `;
+        // linhaJogos.listaNaoOrdenada.innerHTML = stringLinhas;
 
+        stringHtml += `<h3>${dados[i].genre}</h3>
+                      <ul>${stringLinhas}</ul>`;
 
-        /*
-                for (j = 0; j < 3; j++) {
-        
-                    let itemLista = document.createElement("li");
-                    let imagemJogo = document.createElement("img");
-                    let tituloPequeno = document.createElement("small");
-        
-                    listaNaoOrdenada.appendChild(itemLista);
-                    itemLista.appendChild(imagemJogo);
-                    itemLista.appendChild(tituloPequeno);
-        
-                    imagemJogo.setAttribute("src", dados[controle].thumbnail);
-                    imagemJogo.setAttribute("alt", dados[controle].title);
-                    tituloPequeno.innerText = dados[controle].title;
-        
-                    controle++;
-                }*/
+        // containerDeJogos.linhaJogos.push(linhaJogos);
     }
+
+    containerDeJogos.jogos.innerHTML = stringHtml;
+
+    listaContainerDeJogos.push(containerDeJogos);
+
+    console.log(listaContainerDeJogos);
+}
+
+function limparTela() {
+
+    listaExibida.removeChild(listaContainerDeJogos[listaContainerDeJogos.length - 1].jogos);
+    listaContainerDeJogos.pop();
+    controle = 0;
+
 }
