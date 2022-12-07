d788c3a23ac2249a3ce2961b6c8df5463f3edaca
Thiago-cavalcante-oliveira <thhyagofoz@gmail.com>
2022-12-07 15:55:24 -0300

    finalizada parte da lista de preferidos

--- js/thiago.js
+++ js/thiago.js
@@ -1,21 +1,36 @@
-let preferidos=[540];
+let preferidos=[];
 let lista_favoritos=[];
-const button_add_favorites = document.getElementById("input_preferidos");
+const pagina = document.getElementsByTagName("main");
+const closeFavoritesDiv = document.getElementById("closeFavoritesDiv");
+const button_add_favorites = document.getElementById("PopupFavoritos");
 const showFavorites = document.getElementById("favoritos");
+const closeFavorites = document.getElementById("close_favorites");
+const pai = document.getElementById("ListaFavoritos");
 
 
 function push_id(id){
-    preferidos.push(id);
-    alert("adicionado aos favoritos!!");
-    print_favorites();
-
+   if(preferidos.includes(id) === true ){
+        alert("já adicionado")
+    }else {
+        preferidos.push(id);
+        alert("Jogo adicionado a lista dos favoritos!!");
+        print_favorites();
+   }
 }
 
-function print_favorites() {
+function removeChild(){
+    while(pai.firstChild){
+        pai.removeChild(pai.firstChild)
+    }
+}
+
+function print_favorites() {    
     let index;
+    removeChild();
     for (index in preferidos) {
-        const pai = document.getElementById("ListaFavoritos");
-        const filho = document.createElement("div");
+    const filho = document.createElement("div");
+        
+
 
         const dadosRece = fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${preferidos[index]}`, {
             method: "GET",
@@ -26,27 +41,48 @@ function print_favorites() {
         }).then((response) => {
             response.json().then(dados => {
                 lista_favoritos = dados;
+               
                 console.log(lista_favoritos);
                 console.log("clicou");
-
+                
+                
                 filho.innerHTML = `
-        <img class="ThiagoImgFavoritos" src=" ${lista_favoritos.thumbnail}">
-        
-        `
-                pai.appendChild(filho);
+        <div class="thiagoOrganizafavoritos">
+            <a href="${lista_favoritos.game_url}" class="thiagolink_favorites">
+                <img  class="ThiagoImgFavoritos" src=" ${lista_favoritos.thumbnail}" >
+            </a>
+            <div>
+                <p class="thiagoname_favorite"> ${lista_favoritos.title}</p>
+                <p class="thiagoname_favorite"></p>
+            </div>
+        </div>
+        `              
+              pai.appendChild(filho);
                 });
         }).catch((err) => {
             console.log("Erro!");
         });
 
+        
     }
 }
 
 
-function   popup_favorites(){
-    
-    
-   
+
+function   openPopupFavorites(){
+    button_add_favorites.style.display = 'flex';
+    closeFavoritesDiv.style.display="block";    
+    pagina[0].style.filter = "blur(10px)";
+    console.log("chamou");   
 }
 
-showFavorites.addEventListener("click", popup_favorites);
+function closePopupFavorite(){
+    button_add_favorites.style.display = 'none';
+    pagina[0].style.filter = "none";
+    closeFavoritesDiv.style.display ="none";
+    console.log("fechar");
+}
+
+showFavorites.addEventListener("click", openPopupFavorites);
+closeFavorites.addEventListener("click", closePopupFavorite);
+closeFavoritesDiv.addEventListener("click", closePopupFavorite);
