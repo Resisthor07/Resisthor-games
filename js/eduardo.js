401e2152e131c5e2e9b953bd026726192c0af582
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-07 09:00:00 -0300

    alterado a forma de pesquisar no menu

--- js/eduardo.js
+++ js/eduardo.js
@@ -6,185 +6,99 @@ let varAcao;
 let acao=0;
 let decimoItem=0;
 
-let bot_action = document.getElementById("bot_action");
-bot_action = addEventListener ('click', selecionaAcao);
+let botao =[
+	document.getElementById("bot_action"),
+	document.getElementById("bot_battle"),
+	document.getElementById("bot_figtht"),
+	document.getElementById("bot_racing"),
+	document.getElementById("bot_sci"),
+	document.getElementById("bot_shooting"),
+	document.getElementById("bot_sports"),
+	document.getElementById("bot_strategy")
+];
 
 
-let bot_battle = document.getElementById("bot_battle");
-bot_battle = addEventListener ('click', batalha);
-
-let bot_figtht = document.getElementById("bot_figtht");
-bot_figtht = addEventListener ('click', luta);
-
-let bot_racing = document.getElementById("bot_racing");
-bot_racing = addEventListener ('click', corrida);
-
-let bot_sci = document.getElementById("bot_sci");
-bot_sci = addEventListener ('click', sci);
-
-let bot_shooting = document.getElementById("bot_shooting");
-bot_shooting = addEventListener ('click', tiro );
-
-let bot_sports = document.getElementById("bot_sports");
-bot_sports = addEventListener ('click', esportes);
-
-let bot_strategy = document.getElementById("bot_strategy");
-bot_strategy = addEventListener ('click', estrategia );
+botao[0].addEventListener("click", () => selecionaFiltro(1));
+botao[1].addEventListener("click", () => selecionaFiltro(2));
+botao[2].addEventListener("click", () => selecionaFiltro(3));
+botao[3].addEventListener("click", () => selecionaFiltro(4));
+botao[4].addEventListener("click", () => selecionaFiltro(5));
+botao[5].addEventListener("click", () => selecionaFiltro(6));
+botao[6].addEventListener("click", () => selecionaFiltro(7));
+botao[7].addEventListener("click", () => selecionaFiltro(8));
 
 
+function selecionaFiltro(opc)
+{
+    if(opc==1)
+    {
+        menu = "action";
+        selecionaOpc();
+        return;
+    }
+    if(opc==2)
+    {
+        menu = "Battle-Royale";
+        selecionaOpc();
+        return;
+    }
+    if(opc==3)
+    {
+        menu = "fighting";
+        selecionaOpc();
+        return;
+    }
+	if(opc==4)
+    {
+        menu = "racing";
+        selecionaOpc();
+        return;
+    }
+	if(opc==5)
+    {
+        menu = "sci-fi";
+        selecionaOpc();
+        return;
+    }
+    if(opc==6)
+    {
+        menu = "shooter";
+        selecionaOpc();
+        return;
+    }
+    if(opc==7)
+    {
+        menu = "sports";
+        selecionaOpc();
+        return;
+    } if(opc==8)
+    {
+        menu = "strategy";
+        selecionaOpc();
+        return;
+    }
+    
+    
+}
 
-function selecionaAcao(){
+function selecionaOpc(){
 	
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=action', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-function batalha(){
-
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=Battle-Royale', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-
-function luta(){
-
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=fighting', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-function corrida(){
-
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=racing', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-function sci(){
-
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=sci-fi', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-
-function tiro(){
-
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-function esportes(){ 
-fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=sports', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        varAcao = dados;
-        carregarMais(varAcao, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
-
-}
-
-function estrategia(){ 
-	fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=strategy', {
-		method: "GET",
+	const options = {
+		
+		method: 'GET',
+		
 		headers: {
-			'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
+			'X-RapidAPI-Key': '6180cc780dmshc10d83878e62d71p104deejsn359ac2c41450',
 			'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
 		}
-	}).then((response) => {
-		response.json().then(dados => {
-			varAcao = dados;
-			carregarMais(varAcao, filtro);
-		});
-	}).catch((err) => {
-		console.log("Erro!");
-	});
-	
-}
+	};
 
+	fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${menu}`, options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+
+}
 /*
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', {
     method: "GET",
