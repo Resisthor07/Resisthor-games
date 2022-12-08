d6368b8c9ee0198e8e8e86b640a7a9a6b825202f
SamirCharleston <samircharleston@gmail.com>
2022-12-08 20:16:46 -0300

    adicionado o teste

--- js/samir.js
+++ js/samir.js
@@ -1,17 +1,17 @@
-const dadosRece = fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-}).then((response) => {
-    response.json().then(dados => {
-        dadosServidor = dados;
-        carregarMais(dadosServidor, filtro);
-    });
-}).catch((err) => {
-    console.log("Erro!");
-});
+// const dadosRece = fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', {
+//     method: "GET",
+//     headers: {
+//         'X-RapidAPI-Key': 'cb269c757dmshd8dc4b1605c922dp186c0djsn0a6338dca307',
+//         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+//     }
+// }).then((response) => {
+//     response.json().then(dados => {
+//         dadosServidor = dados;
+//         carregarMais(dadosServidor, filtro);
+//     });
+// }).catch((err) => {
+//     console.log("Erro!");
+// });
 
 let listaExibida = document.getElementById("principal");
 let containerDeJogos = document.createElement("div");
