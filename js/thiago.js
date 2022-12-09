1521b206ea43ffcacbfba1dc2d767862071d1c73
SamirCharleston <samircharleston@gmail.com>
2022-12-09 17:03:02 -0300

    adicionando variavel global dadosServidorInicio

--- js/thiago.js
+++ js/thiago.js
@@ -1,4 +1,4 @@
-let preferidos=[];
+let preferidos = [];
 let lista_favoritos;
 const pagina = document.getElementsByTagName("main");
 const closeFavoritesDiv = document.getElementById("closeFavoritesDiv");
@@ -8,37 +8,38 @@ const removeLista = document.getElementById("remove_lista");
 const closeFavorites = document.getElementById("close_favorites");
 const pai = document.getElementById("ListaFavoritos");
 
-window.onload = function(){
-    if (localStorage.getItem("lista_jogos") != null){
+window.onload = function () {
+    if (localStorage.getItem("lista_jogos") != null) {
         preferidos = JSON.parse(localStorage.getItem("lista_jogos") || "[]");
         console.log(preferidos);
         print_favorites();
     }
 
 }
-function push_id(id){
-   if(preferidos.includes(id) === true ){
+function push_id(id) {
+    if (preferidos.includes(id) === true) {
         alert("Este jogo já está adicionado a sua lista de preferidos");
-        
-    }else {
-        preferidos.push(id);        
+
+    } else {
+        preferidos.push(id);
         alert("Jogo adicionado a lista dos favoritos!!");
         print_favorites();
-       localStorage.setItem("lista_jogos", JSON.stringify(preferidos));
-   }
+        localStorage.setItem("lista_jogos", JSON.stringify(preferidos));
+    }
 }
 
-function removeChild(){
-    while(pai.firstChild){
+function removeChild() {
+    while (pai.firstChild) {
         pai.removeChild(pai.firstChild)
     }
 }
 
-function print_favorites() {    
+function print_favorites() {
     let index;
     removeChild();
+
     for (index in preferidos) {
-    const filho = document.createElement("div");
+        const filho = document.createElement("div");
 
         const dadosRece = fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${preferidos[index]}`, {
             method: "GET",
@@ -48,10 +49,10 @@ function print_favorites() {
             }
         }).then((response) => {
             response.json().then(dados => {
-                lista_favoritos = dados;               
+                lista_favoritos = dados;
                 console.log(lista_favoritos);
                 console.log("clicou");
-                
+
                 filho.innerHTML = `
         <div class="thiagoOrganizafavoritos">
             <a href="${lista_favoritos.game_url}" class="thiagolink_favorites">
@@ -65,42 +66,42 @@ function print_favorites() {
                 alt="lixeira"  id="remove_lista" onclick=" remove_lista(lista_favoritos.id)">
             </div>
         </div>
-        `              
-              pai.appendChild(filho);
-               
-                });
-            
+        `
+                pai.appendChild(filho);
+
+            });
+
         }).catch((err) => {
             console.log("Erro!");
         });
 
-        
+
     }
 }
 
-function   openPopupFavorites(){
+function openPopupFavorites() {
     button_add_favorites.style.display = 'flex';
-    closeFavoritesDiv.style.display="block";    
+    closeFavoritesDiv.style.display = "block";
     pagina[0].style.filter = "blur(10px)";
-    console.log("chamou");   
+    console.log("chamou");
 }
-function closePopupFavorite(){
+function closePopupFavorite() {
     button_add_favorites.style.display = 'none';
     pagina[0].style.filter = "none";
-    closeFavoritesDiv.style.display ="none";
+    closeFavoritesDiv.style.display = "none";
     mudaEstiloRafael(3);
     console.log("fechar");
 }
 
-function remove_lista(id){
+function remove_lista(id) {
     let index = preferidos.indexOf(id);
-    if(index !== -1){
-    console.log("lixeira");
-    preferidos.splice(index,1);
-    print_favorites();
+    if (index !== -1) {
+        console.log("lixeira");
+        preferidos.splice(index, 1);
+        print_favorites();
     }
-    
-    
+
+
 }
 
 //removeLista.addEventListener("click", remove_lista);
