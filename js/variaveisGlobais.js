591cf5d0a0f7f32899c83ba200e962906b7af490
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2022-12-12 20:13:37 -0300

    inseri minhas variaveis em globais

--- js/variaveisGlobais.js
+++ js/variaveisGlobais.js
@@ -62,3 +62,49 @@ elementosRafael[2].addEventListener("click", () => selecionaOpcRafael(2));
 elementosRafael[3].addEventListener("click", () => selecionaOpcRafael(3));
 
 /*Fim variaveis globais Rafael*/
+
+/* Variaveis Globais Eduardo*/
+
+let decimoItem=0;
+let menu;
+let boolEduardo;
+let controlaFiltro=0;
+
+let divBot = [
+    document.getElementById("action"),
+    document.getElementById("battle"),
+    document.getElementById("fight"),
+    document.getElementById("racing"),
+    document.getElementById("sci"),
+    document.getElementById("shooting"),
+    document.getElementById("sports"),
+    document.getElementById("strategy")
+];
+
+let botao = [
+    document.getElementById("bot_action"),
+    document.getElementById("bot_battle"),
+    document.getElementById("bot_figtht"),
+    document.getElementById("bot_racing"),
+    document.getElementById("bot_sci"),
+    document.getElementById("bot_shooting"),
+    document.getElementById("bot_sports"),
+    document.getElementById("bot_strategy")
+];
+
+let botHome = document.getElementById("bot_home");
+botHome.addEventListener("click", () => selecionaFiltro(9));
+
+let divHome = document.getElementById("area_home");
+
+
+botao[0].addEventListener("click", () => selecionaFiltro(1));
+botao[1].addEventListener("click", () => selecionaFiltro(2));
+botao[2].addEventListener("click", () => selecionaFiltro(3));
+botao[3].addEventListener("click", () => selecionaFiltro(4));
+botao[4].addEventListener("click", () => selecionaFiltro(5));
+botao[5].addEventListener("click", () => selecionaFiltro(6));
+botao[6].addEventListener("click", () => selecionaFiltro(7));
+botao[7].addEventListener("click", () => selecionaFiltro(8));
+
+//Fim das Variaveis globais Eduardo
